589ff1b05d44a40204a061db27c918946ddf1e97
TEJAS INDURKAR <indurkartejas786@gmail.com>
2024-06-22 15:23:42 +0530

    first commit

---
+++ MERN Authentication/frontend/src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import { Container, Card, Button } from 'react-bootstrap';
+
+const Hero = () => {
+  return (
+    <div className=' py-5'>
+      <Container className='d-flex justify-content-center'>
+        <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
+          <h1 className='text-center mb-4'>USER AUTHENTICATION</h1>
+          <p className='text-center mb-4'>
+            WEL-COME To User Authentication System You Can SignUp 
+            or Login Your Profile Also Update
+          </p>
+          <div className='d-flex'>
+            <Button variant='primary' href='/login' className='me-3'>
+              Sign In
+            </Button>
+            <Button variant='secondary' href='/register'>
+              Register
+            </Button>
+          </div>
+        </Card>
+      </Container>
+    </div>
+  );
+};
+
+export default Hero;
